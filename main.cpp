ce96564e25531897fa3b97f0ec7dc59577485a76
B-J-05 <ben@sienatech.com>
2024-10-04 14:29:29 -0400

    InitialCode
    Just the first bit of code

---
+++ main.cpp
@@ -0,0 +1,26 @@
+// Hello world!
+
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <cassert>
+#include <cstdlib>
+
+int main(int argc, char* argv[]) {
+
+  // process command line arguments
+  /*if (argc != 3) {
+    std::cerr << "ERROR: incorrect number of command line arguments" << std::endl;
+    std::cerr << "USAGE:   " << argv[0] << " <input_file> <output_file>" << std::endl;
+    exit(1);
+  }
+  // open the input file for parsing
+  std::ifstream istr(argv[1]);
+  if (!istr) {
+    std::cerr << "ERROR: could not open input file " << argv[1] << std::endl;
+    exit(1);
+  }*/
+  
+  std::cout << "hello!" << std::endl;
+  
+}
